f8d69ba94d690b19a2fe0e6af6dbe5f0989cc338
Kadyca <peterk@solartechnicalservices.com>
2025-06-02 23:06:54 -0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -103,7 +103,7 @@ function App() {
         <Paper shadow="sm" p="md" withBorder>
           <Stack spacing="md">
             {error && (
-              <Alert color="red" title="Error">
+              <Alert color="red\" title="Error">
                 {error}
               </Alert>
             )}
